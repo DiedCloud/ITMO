86bb5708e67144e398d812ba77a043ecf4dedabd
DiedCloud <fkirill2604@gmail.com>
2024-06-13 17:25:50 +0300

    fix: for ant on helios we need to write it like this

--- Web/Lab3/src/test/java/AreaCheckerTest.java
+++ Web/Lab3/src/test/java/AreaCheckerTest.java
@@ -1,18 +1,25 @@
 import model.AreaChecker;
+/*
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+*/
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class AreaCheckerTest {
     @Test
     public void controllerNotHit() {
         boolean result = AreaChecker.getResult(1, 1, 1);
         System.out.println("Hit Test: " + result);
-        Assertions.assertFalse(result);
+        // Assertions.
+        assertFalse(result);
     }
     @Test
     public void controllerHit() {
         boolean result = AreaChecker.getResult(-1.7, 1.2, 3);
         System.out.println("Hit Test: " + result);
-        Assertions.assertTrue(result);
+        // Assertions.
+        assertTrue(result);
     }
 }
