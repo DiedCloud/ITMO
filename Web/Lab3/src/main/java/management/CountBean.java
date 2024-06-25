e91d0ba78d7e8f96ac43e7910cde82dfd18f5538
DiedCloud <fkirill2604@gmail.com>
2024-06-25 13:43:12 +0300

    feat(opi4): notifications in CountBean

--- Web/Lab3/src/main/java/management/CountBean.java
+++ Web/Lab3/src/main/java/management/CountBean.java
@@ -3,10 +3,12 @@ package management;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.management.*;
+
 /**
  * Managed bean для получения информации о количестве точек
  */
-public class CountBean implements CountMBean {
+public class CountBean implements CountMBean, NotificationBroadcaster {
     @Getter
     @Setter
     long resultsCount;
@@ -14,10 +16,40 @@ public class CountBean implements CountMBean {
     @Setter
     long resultsHitCount;
 
+    private final NotificationBroadcasterSupport broadcaster = new NotificationBroadcasterSupport();
+
     @Override
-    public void registerNewPoint(boolean hit) {
+    public synchronized void registerNewPoint(boolean hit) {
         setResultsCount(resultsCount+1);
         if (hit)
             setResultsHitCount(resultsHitCount+1);
+
+        if (!hit) {
+            Notification n = new Notification(
+                    "miss",
+                    this,
+                    System.currentTimeMillis(),
+                    "Not in target!");
+
+            broadcaster.sendNotification(n);
+        }
+    }
+
+    @Override
+    public void addNotificationListener(NotificationListener listener, NotificationFilter filter, Object handback) throws IllegalArgumentException {
+        broadcaster.addNotificationListener(listener, filter, handback);
+    }
+
+    @Override
+    public void removeNotificationListener(NotificationListener listener) throws ListenerNotFoundException {
+        broadcaster.removeNotificationListener(listener);
+    }
+
+    @Override
+    public MBeanNotificationInfo[] getNotificationInfo() {
+        String[] types = new String[] { "miss" };
+        String name = Notification.class.getName();
+        String description = "Notification sent when miss recorded";
+        return new MBeanNotificationInfo[] { new MBeanNotificationInfo(types, name, description) };
     }
 }
